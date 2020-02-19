8e6cd677f12ea855e905f16a1c66f14a33db7988
Caortega4 <53986225+Caortega4@users.noreply.github.com>
2020-02-19 14:31:31 -0700

    Finished findTailExhaustive

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -22,7 +22,80 @@ public class SnakeGame {
         headPosition[1] = head[1];
     }
 
-    public int[] findTailExhaustive(){
+    public int[] findTailExhaustive() {
+        resetCounters();
+        int count = 0;
+        int length = 0;
+        int prevRow;
+        int prevCol;
+        int nextCol;
+        int nextRow;
+        int[] outArray = new int[3];
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                exhaustiveChecks++;
+                //If the cell is false, jump to the next cell
+                if (!game[i][j]) continue;
 
+                prevRow = i - 1;
+                prevCol = j - 1;
+                nextCol = j + 1;
+                nextRow = i + 1;
+
+
+                //If it gets here the cell is true, increment the length
+                length++;
+                //If statements to check if the cell's neighbors are not out of bounds
+
+
+                if (i != 0) {
+                    if (game[prevRow][j] == true) count++;
+                }
+
+                if (j != 0) {
+                    if (game[i][prevCol] == true) count++;
+                }
+
+                if (j != game.length - 1) {
+                    if (game[i][nextCol] == true) count++;
+                }
+
+                if (i != game.length - 1) {
+                    if (game[nextRow][j] == true) count++;
+                }
+
+                if (count == 1 && (i != headPosition[0] && j != headPosition[1])) {
+                    outArray[0] = i;
+                    outArray[1] = j;
+                }
+
+
+
+
+
+                count = 0;
+            }
+        }
+        outArray[2] = length;
+
+        return outArray;
+
+
+
+    }
+
+    /*
+    public int[] findTailRecursive(){
+        int length = 0;
+        length++;
+        resetCounters();
+
+    }
+    */
+
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
     }
 }
